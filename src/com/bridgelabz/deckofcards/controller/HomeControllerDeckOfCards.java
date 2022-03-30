ad4df12e2ecef8a79680b704e22415b9d25b8174
mangesh9903 <mangeshb9903@gmail.com>
2022-03-30 08:35:42 +0530

    Welcome to Deck Of Cards.

---
+++ src/com/bridgelabz/deckofcards/controller/HomeControllerDeckOfCards.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.deckofcards.controller;
+
+public class HomeControllerDeckOfCards {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Deck Of Cards Game.");
+    }
+}
